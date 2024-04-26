ed580ae1a612f90b31255c90c41f868da7ef6961
Matan Konstantini <matanko35@gmail.com>
2024-04-26 17:11:45 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Portal Coffee
+
+Get your coffe via portal.
+The new technology to get your coffe from home, hot and ready.
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/App.scss'
+// import Logo from './assets/Logo.svg'
+
+function App() {
+
+  return (
+    <>
+      <h1>Portal Coffee</h1>
+    </>
+  )
+}
+
+export default App
